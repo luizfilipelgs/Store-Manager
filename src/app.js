62cb2537eafb383f4f549ba148de89017bd21ca2
luizfilipelgs <luizfilipelgs@gmail.com>
2022-11-18 19:02:37 -0300

    feat: connections

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
@@ -11,7 +12,5 @@ app.get('/', (_request, response) => {
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação
-
-
 
 module.exports = app;
