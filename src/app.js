36349d81058de009b5d9409bde8cb3e75ac6068f
luizfilipelgs <luizfilipelgs@gmail.com>
2022-11-18 17:35:09 -0300

    start project

--- src/app.js
+++ src/app.js
@@ -11,4 +11,6 @@ app.get('/', (_request, response) => {
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação
 
+
+
 module.exports = app;
