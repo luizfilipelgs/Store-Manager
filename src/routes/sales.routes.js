ceb8b25e6a0ffe07401f1c3e58ece5a9ca3ac3b0
luizfilipelgs <luizfilipelgs@gmail.com>
2022-11-21 17:41:28 -0300

    feat middle registreSales

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const salesController = require('../controllers/sales.controller');
+const { validateSale } = require('../middlewares/products.middlewares');
 
 const salesRouter = express.Router();
 
 // salesRouter.get('/', salesController.getAllSales);
 // salesRouter.get('/:id', salesController.getSaleID);
-salesRouter.post('/', salesController.registreSales);
+salesRouter.post('/', validateSale, salesController.registreSales);
 
 module.exports = salesRouter;
